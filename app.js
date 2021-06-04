e0ebf3dd7b004f080a25bb8e09feeeb4865adbdc
Cristian Montes <cmontesmujica@gmail.com>
2021-06-04 07:41:32 -0700

    DOM elements and imgs added

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 // import functions
 
 // reference needed DOM elements
-
+const userInput = document.getElementById('user-input');
+const submitBtn = document.getElementById('submit-btn');
+const oddPic = document.getElementById('odd-pic');
+const evenPic = document.getElementById('evenPic');
 
 // set event listeners 
+
   // get user input(s)
   // do any needed work with the value(s)
   // update DOM to reflect new value(s)
